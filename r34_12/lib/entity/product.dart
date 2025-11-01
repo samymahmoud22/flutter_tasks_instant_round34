bca1e20144a0c7f9002fee5de0043753958ff7a9
Salsabeel Ibrahim <Salsabeeladam281@gmail.com>
2025-11-01 20:47:09 +0200

    Add second task to project

---
+++ r34_12/lib/entity/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String image;
+  final String name;
+  final String status;
+  final String price;
+
+  Product({
+    required this.image,
+    required this.name,
+    required this.status,
+    required this.price,
+  });
+}
